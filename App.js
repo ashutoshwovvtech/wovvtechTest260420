e0e2f141866bed89b43f4dd175051ba23426844b
Sandeep Patel <sandeep.patel@v2stech.com>
2020-04-26 23:54:47 +0530

    souce added

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default App;
